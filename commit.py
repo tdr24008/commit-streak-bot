655c87c863d3a384aa31ba056c56dc21d4b8c7c4
Tom <tom.richardson824@gmail.com>
2025-07-03 14:51:47 +0100

    Set up commit streak bot

---
+++ commit.py
@@ -0,0 +1,16 @@
+import os
+import random
+import datetime
+import subprocess
+
+# Generate 1 to 5 commits
+num_commits = random.randint(1, 5)
+
+for i in range(num_commits):
+    # Write to a dummy file
+    with open("log.txt", "a") as f:
+        f.write(f"{datetime.datetime.now()}: Commit number {i+1}\n")
+
+    # Git commands
+    subprocess.run(["git", "add", "log.txt"])
+    subprocess.run(["git", "commit", "-m", f"Automated commit {i+1} of {num_commits}"])
